f0030656fe696a885c0585b7fd9f46e84703dd19
Erick Walker <selfmade60@Ericks-MacBook-Pro.local>
2020-08-20 22:53:28 -0400

    changes

---
+++ src/components/app/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+import Navigation from '../Navigation/Navigation'
+import Welcome from '../../components/welcome/Welcome'
+import Clock from '../../components/Clock/Clock'
+import Contact from '../Contact/Contact'
+import Page404 from '../errorPage/errorPage'
+
+
+
+function App() {
+
+  return (
+    <div className = "App">
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path = "/"
+          render = {(props) => <Welcome {...props} name = "Erick" />}
+        />
+
+        <Route
+          exact
+          path = "/welcome/:name"
+          render = {(props) => <Welcome {...props} name = {props.match.params.name} />}
+        />
+
+        <Route
+          exact
+          path = "/clock"
+          component = {Clock}
+        />
+
+
+        <Route
+          exact
+          path = "/contact"
+          component = {Contact}
+        />
+
+        <Route 
+          exact = {true}
+          path = "*"
+          component = {Page404}
+        />
+      </Switch>
+
+
+    </div>
+  );
+}
+
+export default App;
